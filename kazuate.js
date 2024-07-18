7226550fa71f5e4ec8222a17952f199b5923cc82
G386762023 <238676@st.takushoku-u.ac.jp>
2024-07-18 14:46:22 +0900

    あ

--- kazuate.js
+++ kazuate.js
@@ -2,7 +2,7 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 0;
+let kaisu = 5;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
@@ -16,7 +16,10 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+  console.log(kaisu+'回目の予想:')
+  if(kaisu >= 4 ) {
+    console.log('答えは'+kotae+'でした．すでにゲームは終わっています');
+  }
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
